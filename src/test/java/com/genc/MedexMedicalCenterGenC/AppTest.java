3639b7d953f4aa3dcfd827c0d238ddcd2937a5b8
798011 <798011@ctsaus-lt-471.cts.com>
2019-08-09 10:52:10 +1000

    Updated POM File and added starter test file

--- src/test/java/com/genc/MedexMedicalCenterGenC/AppTest.java
+++ src/test/java/com/genc/MedexMedicalCenterGenC/AppTest.java
@@ -1,38 +1,40 @@
+/*
+ * This is just a test SeleniumUnit test file
+ * --- DO NOT WRITE YOUR CODE IN HERE ----
+ * Please make a new JUnit test case file for your Selenium test cases
+ * Run this test file to check if selenium is working on your local repository
+*/
+
 package com.genc.MedexMedicalCenterGenC;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	WebDriver driver;
+	String baseUrl;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@BeforeEach
+	void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+		driver = new ChromeDriver();
+		baseUrl = "http://ctsc00849530701:9000/MedexMedicalCenter/";
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		driver = null;
+	}
+
+	@Test
+	void test() throws InterruptedException {
+		driver.get(baseUrl);
+		Thread.sleep(2000);
+		driver.close();
+	}
+
 }
