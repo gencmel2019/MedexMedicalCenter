d3ce758294c2797b2343efaffe6ab9a39be744a4
798011 <798011@ctsaus-lt-471.cts.com>
2019-08-09 15:37:16 +1000

    Added TC01 Test case and git ignore file

---
+++ src/test/java/com/genc/MedexMedicalCenterGenC/TC06CancelAppointment.java
@@ -0,0 +1,24 @@
+package com.genc.MedexMedicalCenterGenC;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TC06CancelAppointment {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCancelAppointment() {
+		fail("Not yet implemented");
+	}
+
+}
